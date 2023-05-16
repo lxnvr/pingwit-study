55db53c8e0f444f27ba41181918ec52cabdd010b
lxnvr <1d@tut.by>
2023-05-16 04:28:44 +0300

    lesson 3 + homework v3

--- les3/homework/Task5TypeEven.java
+++ les3/homework/Task5TypeEven.java
@@ -4,13 +4,11 @@ package org.example.les3.homework;
 Task 5
 Напишите программу вывода всех четных чисел от 2 до 100 включительно
  */
-public class Task5SumEven {
+public class Task5TypeEven {
     public static void main(String[] args) {
-        int sumEven = 0;
-
+        System.out.print("Even row is ");
         for (int i = 2; i <= 100; i += 2) {
-            sumEven = sumEven + i;
+            System.out.print(i + ", ");
         }
-        System.out.println("Sum of even from 2 to 100 is " + sumEven);
     }
 }
