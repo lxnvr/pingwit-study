22855aabfdb119d943ec9927c455c316cb889252
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-17 21:42:32 +0300

    Update Task2Sum.java

--- les3/homework/Task2Sum.java
+++ les3/homework/Task2Sum.java
@@ -4,12 +4,13 @@ package org.example.les3.homework;
 Task 2
 Вычислить: 1+2+4+8+...+256
  */
+//+
 public class Task2Sum {
     public static void main(String[] args) {
 
         int sum = 0;
         for (int i = 1; i <= 256; i *= 2) {
-            sum = sum + i;
+            sum = sum + i;//sum += i as an option
         }
         System.out.println("1+2+4+...+256 = " + sum);
     }
