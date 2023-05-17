6389199b694f9c816a083e2122b4dabf89096ef8
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-17 21:59:28 +0300

    Update Task5TypeEven.java

--- les3/homework/Task5TypeEven.java
+++ les3/homework/Task5TypeEven.java
@@ -8,7 +8,7 @@ public class Task5TypeEven {
     public static void main(String[] args) {
         System.out.print("Even row is ");
         for (int i = 2; i <= 100; i += 2) {
-            System.out.print(i + ", ");
+            System.out.print(i + ", ");//а может уберем запятую после последнего числа?:)
         }
     }
 }
