3195c6366bd2506def33e9a10aba9da54e42bb45
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-17 21:58:00 +0300

    Update Task4InchToCm.java

--- les3/homework/Task4InchToCm.java
+++ les3/homework/Task4InchToCm.java
@@ -5,9 +5,11 @@ Task 4
 Напишите программу печати таблицы перевода расстояний из дюймов в сантиметры
 для значений длин от 1 до 20 дюймов. 1 дюйм = 2,54 см
  */
+//+
 public class Task4InchToCm {
     public static void main(String[] args) {
         float cm = 2.54f;
+        //20 - magic number. Его можно вынести в переменную
         for (int inch = 1; inch <= 20; inch++) {
             Float inch2cm = inch * cm;
             String inchVal = String.format("|  %02d in  |", inch);
