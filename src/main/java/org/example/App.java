0afbb4cd2fed9e00796b1963292fead4589786a3
lxnvr <1d@tut.by>
2023-05-19 02:29:08 +0300

    test commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Damn I was born to die!" );
     }
 }
